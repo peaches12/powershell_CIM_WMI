bf1392f000894d55c2d610ae786d71a00fd9152a
peaches12 <vlaz1999@yahoo.com>
2016-09-13 17:08:14 -0400

    2016-09-13: read from csv-file

--- main_01.ps1
+++ main_01.ps1
@@ -1,7 +1,38 @@
 ﻿
 
-$filik_s_hostami = Read-Host -Prompt 'Enter file with hosts (one line one host): '
+$filik_s_hostami = Read-Host -Prompt 'Enter file with hosts (one line one host)'
 
+Write-Host ".."
 Write-Host "..File: $filik_s_hostami"
+Write-Host ".."
 
+
+if (Test-Path $filik_s_hostami ) 
+{ 
+  write-host "-"
+  write-host "CSV FILE ( $filik_s_hostami ) -- EXISTS."
+  write-host "-"
+}
+else
+{
+  write-host "-"
+  write-host "CSV FILE ( $filik_s_hostami ) -- DOESNT EXIST. EXITING."
+  write-host "-"
+  exit
+}
+
+
+$stroki_iz_filika = Import-CSV $filik_s_hostami -delimiter ','
+
+
+
+$stroki_iz_filika | foreach {
+
+    $tek_host = $_.host
+
+    write-host "$tek_host"
+}
+
+
+# Get-CimInstance -ClassName win32_share -ComputerName
 
